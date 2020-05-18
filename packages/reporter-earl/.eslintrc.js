b0abe0b7d377fd4ce0f31711d6e5faddfb490386
michael-siek <michaelsiek094@gmail.com>
2020-05-18 10:01:57 -0400

    fix eslint

--- packages/reporter-earl/.eslintrc.js
+++ packages/reporter-earl/.eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-debugger': 'off',
+    'no-empty-pattern': 'off'
+  },
   overrides: [
     {
       files: 'tests/*.test.ts',
       env: {
         jest: true
-      }
-    },
-    {
-      files: '*.ts',
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        'no-debugger': 'off',
-        'no-empty-pattern': 'off'
       }
     }
   ]
