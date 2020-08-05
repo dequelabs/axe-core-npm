8cc9e1b87600a9a3a3ee7e75390e63512b839255
michael-siek <michaelsiek094@gmail.com>
2020-08-05 13:53:40 -0400

    remove stuff in eslintrc file

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    'no-console': 'off',
     'react/no-find-dom-node': 'off',
-    'no-undef': 'off',
     'react/prop-types': 'off'
   },
   env: {
