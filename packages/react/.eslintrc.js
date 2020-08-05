9560ece6c376498f15925fb6ff7b1e3c5710ea3d
michael-siek <michaelsiek094@gmail.com>
2020-08-05 14:21:23 -0400

    fix lint

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/prop-types': 'off',
     'react/no-find-dom-node': 'off',
-    'react/prop-types': 'off'
+    'no-undef': 'off'
   },
   env: {
     browser: true,
