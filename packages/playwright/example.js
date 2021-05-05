e9008f2523af1bd0bbe55b4b4572c42d6d92c810
michael-siek <michaelsiek094@gmail.com>
2021-05-05 11:43:37 -0400

    fix ci

--- packages/playwright/example.js
+++ packages/playwright/example.js
@@ -5,7 +5,7 @@ const playwright = require('playwright');
   const browser = await playwright.chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://html5-sandbox.glitch.me/');
+  await page.goto('https://dequeuniversity.com/demo/mars/');
   const results = await new AxeBuilder({ page }).analyze();
   console.log(results);
   await browser.close();
