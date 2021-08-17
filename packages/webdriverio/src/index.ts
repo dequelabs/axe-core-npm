8b45b8fffe5058e84bbadd8cfedbd86070e73c93
michael-siek <michaelsiek094@gmail.com>
2021-08-17 12:59:29 -0400

    allowed origins

--- packages/webdriverio/src/index.ts
+++ packages/webdriverio/src/index.ts
@@ -179,6 +179,7 @@ export default class AxeBuilder {
     return `
       ${this.axeSource}
       axe.configure({ 
+        allowedOrigins: ['<unsafe_all_origins>'],
         branding: { application: 'webdriverio' }
       })
       `;
