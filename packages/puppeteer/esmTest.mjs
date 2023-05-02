cf9c6be0931e6918ffdd8b1f633ea894430dd36b
AdnoC <adam.r.cutler@gmail.com>
2023-05-02 09:48:00 -0400

    add test

--- packages/puppeteer/esmTest.mjs
+++ packages/puppeteer/esmTest.mjs
@@ -1,7 +1,26 @@
 import defaultExport, { AxePuppeteer } from './dist/index.mjs';
 import assert from 'assert';
+import puppeteer from 'puppeteer';
+import { fileURLToPath, pathToFileURL } from 'url';
+import { join } from 'path';
 
 const exportIsFunction = typeof(defaultExport) === 'function';
 const exportIsSame = defaultExport === AxePuppeteer;
 assert(exportIsFunction, 'export is not a function');
 assert(exportIsSame, 'default and named export is not the same');
+
+
+async function integrationTest() {
+  let path = fileURLToPath(new URL('.', import.meta.url));
+  path = join(path, './node_modules/axe-test-fixtures/fixtures/index.html');
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setBypassCSP(true);
+  await page.goto(pathToFileURL(path));
+  const results = await new AxePuppeteer(page).analyze();
+  assert(results.violations.length > 0, 'could not find violations');
+  await page.close();
+  await browser.close();
+}
+integrationTest();
