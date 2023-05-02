d1f569a2c62df3f9388c8466e06ed2c9e9b8b2da
AdnoC <adam.r.cutler@gmail.com>
2023-05-02 10:11:07 -0400

    puppeteer whyyyyy

--- packages/puppeteer/esmTest.mjs
+++ packages/puppeteer/esmTest.mjs
@@ -10,11 +10,19 @@ assert(exportIsFunction, 'export is not a function');
 assert(exportIsSame, 'default and named export is not the same');
 
 
+const options = {};
+
+if (process.env.CI) {
+  options.args = [];
+  options.args.push('--no-sandbox', '--disable-setuid-sandbox');
+  options.executablePath = '/usr/bin/google-chrome-stable';
+}
+
 async function integrationTest() {
   let path = fileURLToPath(new URL('.', import.meta.url));
   path = join(path, './node_modules/axe-test-fixtures/fixtures/index.html');
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.setBypassCSP(true);
   await page.goto(pathToFileURL(path));
