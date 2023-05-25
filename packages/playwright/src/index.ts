50eda16e9973651f0a4debba16365f8ff3d49e36
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-25 03:55:41 +0200

    chore(playwright): remove unused variables

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -5,9 +5,7 @@ import type {
   AxeResults,
   SerialContextObject,
   PartialResults,
-  ContextObject,
   SerialSelectorList,
-  SerialSelector,
   SerialFrameSelector
 } from 'axe-core';
 import axe from 'axe-core';
@@ -151,7 +149,7 @@ export default class AxeBuilder {
 
   public async analyze(): Promise<AxeResults> {
     const context = normalizeContext(this.includes, this.excludes);
-    const { page, option: options } = this;
+    const { page } = this;
 
     page.evaluate(this.script());
     const runPartialDefined = await page.evaluate<boolean>(
