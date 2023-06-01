ec2a1deeae3a4c2dbcd801fbcc6a7f0154f1311f
Steven Lambert <2433219+straker@users.noreply.github.com>
2023-06-01 14:42:25 -0600

    Update packages/playwright/example.js
    Co-authored-by: Gabe <41127686+Zidious@users.noreply.github.com>

--- packages/playwright/example.js
+++ packages/playwright/example.js
@@ -2,7 +2,7 @@ const { AxeBuilder } = require('@axe-core/playwright');
 const playwright = require('playwright');
 
 (async () => {
-  const browser = await playwright.chromium.launch();
+  const browser = await playwright.chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://dequeuniversity.com/demo/mars/');
