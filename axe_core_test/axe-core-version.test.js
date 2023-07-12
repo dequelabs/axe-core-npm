c07989136d0c98798682f34ba46c0623ea9d8b97
Stephen Mathieson <mathiesonstephen8@gmail.com>
2023-07-12 15:47:07 -0400

    test earl too

--- axe_core_test/axe-core-version.test.js
+++ axe_core_test/axe-core-version.test.js
@@ -4,20 +4,16 @@ const semver = require('semver');
 
 for (const pkgFile of glob.sync('../packages/*/package.json')) {
   const pkg = require(pkgFile);
-  const axeDependency = pkg.dependencies?.['axe-core'];
-
-  const testFn = axeDependency ? it : it.skip;
+  const axeDependency =
+    pkg.dependencies?.['axe-core'] || pkg.devDependencies?.['axe-core'];
 
   describe(pkg.name, () => {
-    testFn(
-      'should have the same semver major/minor as its axe-core dependency',
-      () => {
-        const axeVersion = semver.parse(semver.coerce(axeDependency));
-        const pkgVersion = semver.parse(pkg.version);
+    it('should have the same semver major/minor as its axe-core dependency', () => {
+      const axeVersion = semver.parse(semver.coerce(axeDependency));
+      const pkgVersion = semver.parse(pkg.version);
 
-        assert.equal(axeVersion.major, pkgVersion.major);
-        assert.equal(axeVersion.minor, pkgVersion.minor);
-      }
-    );
+      assert.equal(axeVersion.major, pkgVersion.major);
+      assert.equal(axeVersion.minor, pkgVersion.minor);
+    });
   });
 }
