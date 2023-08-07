6824caf9dc1c130d1cfc0da1628f81d5b840ee18
Stephen Mathieson <571265+stephenmathieson@users.noreply.github.com>
2023-08-07 17:42:36 -0400

    test: Make axe-core version test more robust (#767)
    - Test every package (under `packages/*`)
    - Use the battle-tested [`semver`](https://www.npmjs.com/package/semver) library rather than a regex

--- axe_core_test/axe-core-version.test.js
+++ axe_core_test/axe-core-version.test.js
@@ -1,47 +1,19 @@
-const { assert } = require('chai');
-const cli = require('../packages/cli/package.json');
-const puppeteer = require('../packages/puppeteer/package.json');
-const reporterEarl = require('../packages/reporter-earl/package.json');
-const webdriverjs = require('../packages/webdriverjs/package.json');
+const assert = require('assert');
+const glob = require('glob');
+const semver = require('semver');
 
-describe('verify axe-core version', () => {
-  const versioning = /([1-9].[0-9]*)/;
+for (const pkgFile of glob.sync('../packages/*/package.json')) {
+  const pkg = require(pkgFile);
+  const axeDependency =
+    pkg.dependencies?.['axe-core'] || pkg.devDependencies?.['axe-core'];
 
-  it('cli', () => {
-    const cliVersion = cli.version;
-    const cliAxeCoreVersion = cli.dependencies['axe-core'];
+  describe(pkg.name, () => {
+    it('should have the same semver major/minor as its axe-core dependency', () => {
+      const axeVersion = semver.parse(semver.coerce(axeDependency));
+      const pkgVersion = semver.parse(pkg.version);
 
-    const [packageAxeCoreVersion] = cliAxeCoreVersion.match(versioning);
-    const [axeCoreCliVersion] = cliVersion.match(versioning);
-    assert.strictEqual(axeCoreCliVersion, packageAxeCoreVersion);
+      assert.equal(axeVersion.major, pkgVersion.major);
+      assert.equal(axeVersion.minor, pkgVersion.minor);
+    });
   });
-
-  it('puppeteer', () => {
-    const puppeteerVersion = puppeteer.version;
-    const puppeteerAxeCoreVersion = puppeteer.dependencies['axe-core'];
-
-    const [packageAxeCoreVersion] = puppeteerAxeCoreVersion.match(versioning);
-    const [axeCorePuppeteerVersion] = puppeteerVersion.match(versioning);
-    assert.strictEqual(axeCorePuppeteerVersion, packageAxeCoreVersion);
-  });
-
-  it('reporter-earl', () => {
-    const reporterEarlVersion = reporterEarl.version;
-    const reporterEarlAxeCoreVersion = reporterEarl.devDependencies['axe-core'];
-
-    const [packageAxeCoreVersion] =
-      reporterEarlAxeCoreVersion.match(versioning);
-    const [axeCoreReporterEarlVersion] = reporterEarlVersion.match(versioning);
-    assert.strictEqual(axeCoreReporterEarlVersion, packageAxeCoreVersion);
-  });
-
-  it('webdriverjs', () => {
-    const webdriverjsVersion = webdriverjs.version;
-    const webdriverjsAxeCoreVersion = webdriverjs.dependencies['axe-core'];
-
-    const [packageAxeCoreVersion] = webdriverjsAxeCoreVersion.match(versioning);
-    const [axeCoreWebdriverjsVersion] = webdriverjsVersion.match(versioning);
-
-    assert.strictEqual(axeCoreWebdriverjsVersion, packageAxeCoreVersion);
-  });
-});
+}
