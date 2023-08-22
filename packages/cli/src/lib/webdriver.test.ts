69e0ba04ed9cffcc63a3b5eec24ef5f7eba019d3
Gabe <41127686+Zidious@users.noreply.github.com>
2023-08-22 17:33:12 +0100

    revert cli changes

--- packages/cli/src/lib/webdriver.test.ts
+++ packages/cli/src/lib/webdriver.test.ts
@@ -3,9 +3,10 @@ import { assert } from 'chai';
 import { startDriver } from './webdriver';
 import { WebDriver } from 'selenium-webdriver';
 import chromedriver from 'chromedriver';
+import chrome from 'selenium-webdriver/chrome';
+import type { Options } from 'selenium-webdriver/chrome';
 import path from 'path';
 import { WebdriverConfigParams } from '../types';
-
 describe('startDriver', () => {
   let config: WebdriverConfigParams;
   let browser: string;
