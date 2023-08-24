5e9d28c1d0532df04b65aed9658e36c0671801af
AdnoC <adam.r.cutler@gmail.com>
2023-08-24 14:13:01 -0400

    upate prep-release script

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -6,3 +6,6 @@ set -e
 releaseLevel="$1"
 
 npx lerna version "$releaseLevel" --conventional-commits --no-push --no-git-tag-version --yes
+version=$(node -pe 'require("./lerna.json").version')
+jq '.version = $newVer' --arg newVer "$version" package.json > tmp
+mv tmp package.json
