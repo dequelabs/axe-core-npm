a7ac806df03e6fc8f5f762adafcd6614798c880e
Gabe <41127686+Zidious@users.noreply.github.com>
2023-10-03 15:18:02 +0100

    ci: update `prepare_release.sh` to take optional release level

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -5,4 +5,10 @@ set -e
 
 releaseLevel="$1"
 
-npx lerna version "$releaseLevel" --conventional-commits --no-push --no-git-tag-version --yes
+# Let lerna handle versioning if "releaseLevel" is not provided.
+if [ -z "$releaseLevel" ] 
+then
+  npx lerna version --conventional-commits --no-push --no-git-tag-version --yes  
+else
+  npx lerna version "$releaseLevel" --conventional-commits --no-push --no-git-tag-version --yes
+fi
