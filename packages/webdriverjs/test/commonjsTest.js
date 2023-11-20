bdb397551cfd8c501bd85dca7bdd6c109b56a7c6
Steven Lambert <2433219+straker@users.noreply.github.com>
2023-11-20 13:13:32 -0700

    support 4.7.3

--- packages/webdriverjs/test/commonjsTest.js
+++ packages/webdriverjs/test/commonjsTest.js
@@ -1,11 +1,25 @@
 // ensure backwards compatibility of commonJs format
-const defaultExport = require('../dist/index.js');
+const implicitDefaultExport = require('../dist/index.js'); // support <4.7.3
+const explicitDefaultExport = require('../dist/index.js').default; // support 4.7.3+
 const { AxeBuilder } = require('../dist/index.js');
 const assert = require('assert');
 
-assert(typeof defaultExport === 'function', 'default export is not a function');
 assert(typeof AxeBuilder === 'function', 'named export is not a function');
+
 assert(
-  defaultExport === AxeBuilder,
-  'default and named export are not the same'
+  typeof implicitDefaultExport === 'function',
+  'implicit default export is not a function'
+);
+assert(
+  implicitDefaultExport === AxeBuilder,
+  'implicit default and named export are not the same'
+);
+
+assert(
+  typeof explicitDefaultExport === 'function',
+  'explicit default export is not a function'
+);
+assert(
+  explicitDefaultExport === AxeBuilder,
+  'explicit default and named export are not the same'
 );
