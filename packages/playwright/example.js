ad0c85bb30f4cc10c13a401403d6417cefc622b5
Scott Ries <scottmries@gmail.com>
2025-07-02 09:40:27 -0400

    removed unrelated changes

--- packages/playwright/example.js
+++ packages/playwright/example.js
@@ -2,22 +2,13 @@ const { AxeBuilder } = require('@axe-core/playwright');
 const playwright = require('playwright');
 
 (async () => {
-  const browser = await playwright.chromium.launch({
-    headless: false,
-    use: {
-      video: 'on',
-      screenshot: 'on',
-      trace: 'on'
-    }
-  });
+  const browser = await playwright.chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://dequeuniversity.com/demo/mars/');
 
   try {
-    const results = await new AxeBuilder({ page })
-      .options({ checks: { 'valid-lang': 'orcish' } })
-      .analyze();
+    const results = await new AxeBuilder({ page }).analyze();
     console.log(results);
   } catch (e) {
     console.error(e);
