b6d50b8e4bc30791b608190ed417d8b17d545340
Jonathan Garbee <jonathan.garbee@deque.com>
2025-08-04 17:26:20 -0400

    conver to substring

--- packages/cli/src/lib/utils.ts
+++ packages/cli/src/lib/utils.ts
@@ -61,26 +61,26 @@ export const parseBrowser = (browser?: string): string | Error => {
   const l = browser.length;
   switch (browser.toLowerCase()) {
     case 'ff':
-    case 'firefox'.substr(0, l):
-    case 'gecko'.substr(0, l):
-    case 'marionette'.substr(0, l):
+    case 'firefox'.substring(0, l):
+    case 'gecko'.substring(0, l):
+    case 'marionette'.substring(0, l):
       return 'firefox';
 
-    case 'chrome'.substr(0, l):
+    case 'chrome'.substring(0, l):
       return 'chrome';
 
     case 'ie':
-    case 'explorer'.substr(0, l):
-    case 'internetexplorer'.substr(0, l):
-    case 'internet_explorer'.substr(0, l):
-    case 'internet-explorer'.substr(0, l):
+    case 'explorer'.substring(0, l):
+    case 'internetexplorer'.substring(0, l):
+    case 'internet_explorer'.substring(0, l):
+    case 'internet-explorer'.substring(0, l):
       return 'ie';
 
-    case 'safari'.substr(0, l):
+    case 'safari'.substring(0, l):
       return 'safari';
 
-    case 'edge'.substr(0, l):
-    case 'microsoftedge'.substr(0, l):
+    case 'edge'.substring(0, l):
+    case 'microsoftedge'.substring(0, l):
       return 'MicrosoftEdge';
 
     default:
